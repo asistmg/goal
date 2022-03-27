b803bc384a9662d8277ea616fe6bbcc1940f719f
asistmg <84500716+asistmg@users.noreply.github.com>
2022-03-27 09:51:47 -0700

    Add files via upload

---
+++ goal/db/delete-goal.php
@@ -0,0 +1,11 @@
+<?php
+if(isset($_GET['id'])){
+      $id=$_GET['id'];
+      $query = "DELETE FROM  goal_table WHERE id = '$id'";
+      include('connect.php');
+      if(mysqli_query($conn, $query)){
+            header("Location:../goal.php?msg=successfull delete");
+      }else{
+            header("Location:../goal.php?msg=".mysqli_error($conn));
+      }
+}
